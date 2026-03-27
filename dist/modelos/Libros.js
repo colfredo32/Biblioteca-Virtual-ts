81ea3ed22afdc4f1ae42689091f5530fbe1655ba
colfredo32 <mortolasantino@gmail.com>
2026-03-27 18:21:44 -0300

    corrijo CI ignorando node_modules y dist

--- dist/modelos/Libros.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Libro {
-    constructor(titulo, autor) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.disponible = true;
-    }
-}
-module.exports = Libro;
